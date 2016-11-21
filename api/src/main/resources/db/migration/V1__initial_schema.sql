aec1120cac4184ea578167298e66b6192346eaf1
Jan Bauer Nielsen <jbn@dbc.dk>
2016-11-21 18:31:20 +0000

    [20701] Added functions for batch and entry access.
    git-svn-id: https://svn.dbc.dk/repos/batch-exchange/trunk@106383 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- api/src/main/resources/db/migration/V1__initial_schema.sql
+++ api/src/main/resources/db/migration/V1__initial_schema.sql
@@ -10,15 +10,16 @@ CREATE TABLE batch (
   name                TEXT,
   status              batch_status NOT NULL DEFAULT 'PENDING',
   timeOfCreation      TIMESTAMP DEFAULT now(),
-  incompleteRecords   INTEGER NOT NULL DEFAULT 0
+  incompleteEntries   INTEGER NOT NULL DEFAULT 0
 );
 CREATE INDEX batch_status_index ON batch(status);
+CREATE INDEX batch_completed_index ON batch(id) WHERE status = 'COMPLETED';
 
-CREATE TYPE record_status AS ENUM ('PENDING','ACTIVE', 'OK', 'FAILED', 'IGNORED');
+CREATE TYPE entry_status AS ENUM ('PENDING','ACTIVE', 'OK', 'FAILED', 'IGNORED');
 
-CREATE TABLE record (
+CREATE TABLE entry (
   id                  SERIAL PRIMARY KEY,
-  status              record_status NOT NULL DEFAULT 'PENDING',
+  status              entry_status NOT NULL DEFAULT 'PENDING',
   timeOfCreation      TIMESTAMP DEFAULT now(),
   timeOfCompletion    TIMESTAMP,
   trackingId          TEXT NOT NULL,
@@ -28,24 +29,24 @@ CREATE TABLE record (
   metadata            JSONB,
   diagnostics         JSONB
 );
-CREATE INDEX record_status_index ON record(status);
-CREATE INDEX record_batch_index ON record(batch);
-CREATE INDEX record_pending_index ON record(id) WHERE status = 'PENDING';
+CREATE INDEX entry_status_index ON entry(status);
+CREATE INDEX entry_batch_index ON entry(batch);
+CREATE INDEX entry_pending_index ON entry(id) WHERE status = 'PENDING';
 
 CREATE FUNCTION update_batch_incomplete() RETURNS trigger AS $$
 DECLARE batch_id INTEGER;
 BEGIN
-  IF NEW.status = 'PENDING'::record_status OR NEW.status = 'ACTIVE'::record_status THEN
-    -- Increment number of incomplete records for batch
+  IF NEW.status = 'PENDING'::entry_status OR NEW.status = 'ACTIVE'::entry_status THEN
+    -- Increment number of incomplete entries for batch
     SELECT id INTO batch_id FROM batch WHERE id = NEW.batch FOR UPDATE;
-    UPDATE batch SET incompleteRecords = incompleteRecords + 1 WHERE id = NEW.batch;
+    UPDATE batch SET incompleteEntries = incompleteEntries + 1 WHERE id = NEW.batch;
   END IF;
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER record_insert_trigger
-  AFTER INSERT ON record
+CREATE TRIGGER entry_insert_trigger
+  AFTER INSERT ON entry
   FOR EACH ROW
   EXECUTE PROCEDURE update_batch_incomplete();
 
@@ -54,18 +55,18 @@ DECLARE batch_id INTEGER;
 DECLARE batch_incomplete INTEGER;
 BEGIN
   IF OLD.timeOfCompletion IS NOT NULL THEN
-    RAISE EXCEPTION 'Cannot update an already completed record';
+    RAISE EXCEPTION 'Cannot update an already completed entry';
   END IF;
 
-  IF (OLD.status = 'PENDING'::record_status OR OLD.status = 'ACTIVE'::record_status)
-     AND NEW.status != 'PENDING'::record_status
-     AND NEW.status != 'ACTIVE'::record_status
+  IF (OLD.status = 'PENDING'::entry_status OR OLD.status = 'ACTIVE'::entry_status)
+     AND NEW.status != 'PENDING'::entry_status
+     AND NEW.status != 'ACTIVE'::entry_status
   THEN
-    -- Mark record as completed, decrement number of incomplete records for batch
+    -- Mark entry as completed, decrement number of incomplete entries for batch
     NEW.timeOfCompletion = clock_timestamp();
     SELECT id INTO batch_id FROM batch WHERE id = NEW.batch FOR UPDATE;
-    UPDATE batch SET incompleteRecords = batch.incompleteRecords - 1 WHERE id = NEW.batch
-      RETURNING incompleteRecords INTO batch_incomplete;
+    UPDATE batch SET incompleteEntries = batch.incompleteEntries - 1 WHERE id = NEW.batch
+      RETURNING incompleteEntries INTO batch_incomplete;
     IF batch_incomplete <= 0 THEN
       UPDATE batch SET status = 'COMPLETED' WHERE id = NEW.batch;
     END IF;
@@ -74,7 +75,26 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER record_status_update_trigger
-  BEFORE UPDATE ON record
+CREATE TRIGGER entry_status_update_trigger
+  BEFORE UPDATE ON entry
   FOR EACH ROW
   EXECUTE PROCEDURE mark_as_completed();
+
+CREATE FUNCTION get_completed_batch() RETURNS SETOF batch AS $$
+  SELECT * FROM batch WHERE status = 'COMPLETED' ORDER BY id LIMIT 1 FOR UPDATE SKIP LOCKED;
+$$ LANGUAGE sql;
+
+CREATE FUNCTION get_batch_entries(batchId INTEGER) RETURNS SETOF entry AS $$
+  SELECT * FROM entry WHERE batch = batchId ORDER BY id;
+$$ LANGUAGE sql;
+
+CREATE FUNCTION claim_pending_entries(maxNumberOfEntries INTEGER) RETURNS SETOF entry AS $$
+  UPDATE entry SET status = 'ACTIVE'
+    WHERE id IN (
+      SELECT id FROM entry WHERE status = 'PENDING' ORDER BY id LIMIT maxNumberOfEntries FOR UPDATE SKIP LOCKED)
+    RETURNING *;
+$$ LANGUAGE sql;
+
+CREATE FUNCTION reset_claimed_entries() RETURNS VOID AS $$
+  UPDATE entry SET status = 'PENDING' WHERE status = 'ACTIVE';
+$$ LANGUAGE sql;
