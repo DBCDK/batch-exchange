edfe77698bef3e491069046da2973e20ca773cd1
Jan Bauer Nielsen <jbn@dbc.dk>
2016-11-22 06:20:06 +0000

    [20701] Added flyway database migrator for batch-exchange.
    git-svn-id: https://svn.dbc.dk/repos/batch-exchange/trunk@106387 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ api/src/main/java/dk/dbc/batchexchange/BatchExchangeDatabaseMigrator.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GNU 3
+ * See license text in LICENSE.txt
+ */
+
+package dk.dbc.batchexchange;
+
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.MigrationInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.sql.DataSource;
+
+@Startup
+@Singleton
+public class BatchExchangeDatabaseMigrator {
+    private static final Logger LOGGER = LoggerFactory.getLogger(BatchExchangeDatabaseMigrator.class);
+
+    @Resource(lookup = "jdbc/batch-exchange")
+    DataSource dataSource;
+
+    public BatchExchangeDatabaseMigrator(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @PostConstruct
+    public void migrate() {
+        final Flyway flyway = new Flyway();
+        flyway.setTable("schema_version");
+        flyway.setBaselineOnMigrate(true);
+        flyway.setDataSource(dataSource);
+        for (MigrationInfo info : flyway.info().all()) {
+            LOGGER.info("database migration {} : {} from file '{}'",
+                    info.getVersion(), info.getDescription(), info.getScript());
+        }
+        flyway.migrate();
+    }
+}
